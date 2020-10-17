5c18ea91b4b288bfacba7c81f5d2b2f46711149f
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-10-17 11:50:32 +0200

    Ajout : IdInconnueException

---
+++ src/main/java/fr/umontpellier/iut/commandes/IdInconnueException.java
@@ -0,0 +1,14 @@
+package fr.umontpellier.iut.commandes;
+
+public class IdInconnueException extends IllegalArgumentException {
+
+    public IdInconnueException(String argo0) {
+        super(argo0);
+	}
+
+	/**
+     *
+     */
+    private static final long serialVersionUID = -510127988136646975L;
+    
+}
