9a2458373996666232442b569a58817de9cb0b56
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-10-25 11:56:12 +0100

    🐛 Correction : Utilisateur n'a pas remplie tous les champs

--- src/main/java/fr/umontpellier/iut/commandes/Send.java
+++ src/main/java/fr/umontpellier/iut/commandes/Send.java
@@ -9,11 +9,14 @@ public interface Send extends Commande {
     @Override
     default void execute(Message messageRecue) {
         String[] messageSplit = messageRecue.getContentRaw().split(" ", 3);
-        try {
-            execute(messageSplit[1], messageSplit[2], messageRecue.getAuthor());
-        } catch (UtilisateurAFaitUneMauvaiseCommandeException e) {
-            messageRecue.getChannel().sendMessage(e.getMessage()).queue();
-        }
+        if (messageSplit.length != 3)
+            messageRecue.getChannel().sendMessage("Je n'ai malheureusement pas comprit votre commande, voici toutes le commandes dont je dispose :\nhttp://commandes.InvisibilityCloak.umontp.fr").queue();
+        else
+            try {
+                execute(messageSplit[1], messageSplit[2], messageRecue.getAuthor());
+            } catch (UtilisateurAFaitUneMauvaiseCommandeException e) {
+                messageRecue.getChannel().sendMessage(e.getMessage()).queue();
+            }
     }
 
     void execute(String textChannel, String message, User user) throws UtilisateurAFaitUneMauvaiseCommandeException;
