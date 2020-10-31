5566deb155ecf43bc1435cfceef81823af636019
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-10-31 14:53:20 +0100

    ♻️ Refactor : SetPseudo.java

--- src/main/java/fr/umontpellier/iut/commandes/setpseudo/SetPseudo.java
+++ src/main/java/fr/umontpellier/iut/commandes/setpseudo/SetPseudo.java
@@ -7,7 +7,7 @@ import fr.umontpellier.iut.commandes.exceptions.UtilisateurAFaitUneMauvaiseComma
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 
-public interface SetPseudo extends Commande {
+interface SetPseudo extends Commande {
 
     @Override
     default void execute(Message messageRecue) {
@@ -16,13 +16,13 @@ public interface SetPseudo extends Commande {
         if (messageSplit.length != 2)
             feedBack = "Je n'ai malheureusement pas compris votre commande, voici toutes les commandes dont je dispose :\nhttp://commandes.InvisibilityCloak.umontp.fr";
         else
-            feedBack = executeSetPseudo(messageRecue.getAuthor(), messageSplit);
+            feedBack = executeSetPseudo(messageSplit[1], messageRecue.getAuthor());
         messageRecue.getChannel().sendMessage(feedBack).queue();
     }
 
-    default String executeSetPseudo(User user, String[] messageSplit) {
+    default String executeSetPseudo(String designationGuilde, User user) {
         try {
-            long idGuild = getIdGuild(messageSplit[1], user);
+            long idGuild = getIdGuild(designationGuilde, user);
             int indexPseudo = BaseDonnee.attribuerNouveauPseudo(user.getIdLong(), idGuild);
             return GestionnairePseudonyme.getMessageNominationParLaCape(indexPseudo);
         } catch (UtilisateurAFaitUneMauvaiseCommandeException e) {
