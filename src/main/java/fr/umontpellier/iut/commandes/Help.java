a557324bfae2bfcbb5a21f20777a4038cce307c1
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-10-31 20:43:06 +0100

    🔨 Mise à jour message d'aide Help

--- src/main/java/fr/umontpellier/iut/commandes/Help.java
+++ src/main/java/fr/umontpellier/iut/commandes/Help.java
@@ -10,6 +10,7 @@ import net.dv8tion.jda.api.entities.Message;
  * @version 1.0
  */
 public class Help implements Commande {
+    public static final String MESSAGE_AIDE = "**Commandes Invisibility Cloak :** \n◻ Commande principale : \n  ▫ `!send {nom-du-channel} {message}` : envoyer un message de manière annonyme sur le channel, en utilisant le nom du channel.\n  ▫ `!sendById {ID-du-channel} {message}` : envoyer un message de manière annonyme sur le channel, en utilisant l'ID du channel.\n  ▫ `!setPseudo {nom-du-serveur}` : permet d'avoir un nouveau pseudonyme sur le serveur.\n  ▫ `!setPseudoById {ID-du-serveur}` : permet d'avoir un nouveau pseudonyme sur le serveur, en utilisant l'ID du serveur.\n◻ Commande secondaire :\n  ▫ `!help` : avoir un apperçu des commandes.\n  ▫ `!feed-back` : faire un retour sur le bot.\n  ▫ `!invite` : pour recevoir le lien pour inviter le bot sur un serveur discord.\n  ▫ `!bug` : faire le retour sur un bug rencontré avec le bot.\n Pour avoir de plus amples détails sur une commande : `!help {nom-de-la-commande}` \nhttps://github.com/DevLab-umontp/InvisibilityCloak.umontp";
 
     /**
      * Envoi un message dans le channel où le message a été reçue.
@@ -21,7 +22,13 @@ public class Help implements Commande {
      */
     @Override
     public void execute(Message messageRecue) {
-        String reponse = "**Commandes Invisibility Cloak :**\nhttps://github.com/DevLab-umontp/InvisibilityCloak.umontp";
+        String reponse = MESSAGE_AIDE;
+        String[] messageSplit = messageRecue.getContentRaw().split(" ", 2);
+        if (messageSplit.length == 2) {
+            Commande commande = RepertoireCommandes.getCommande(messageSplit[1]);
+            if (commande != null)
+                reponse = commande.getMessageAide();
+        }
         messageRecue.getChannel().sendMessage(reponse).queue();
     }
 
