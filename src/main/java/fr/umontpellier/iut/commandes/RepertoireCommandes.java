7b641624e5a0677fb9a0b90a2823f707f5c65ad8
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-10-31 14:55:22 +0100

    🐛 Correction bug : appel aux commandes SetPseudo

--- src/main/java/fr/umontpellier/iut/commandes/RepertoireCommandes.java
+++ src/main/java/fr/umontpellier/iut/commandes/RepertoireCommandes.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 import fr.umontpellier.iut.commandes.send.SendById;
 import fr.umontpellier.iut.commandes.send.SendByName;
+import fr.umontpellier.iut.commandes.setpseudo.SetPseudoIDChannel;
 import fr.umontpellier.iut.commandes.setpseudo.SetPseudoIDGuilde;
 import fr.umontpellier.iut.commandes.setpseudo.SetPseudoNomChannel;
 import fr.umontpellier.iut.commandes.setpseudo.SetPseudoNomGuilde;
@@ -50,11 +51,11 @@ public class RepertoireCommandes {
         REPERTOIRE_DES_COMMANDES.put("sendid", new SendById());
         REPERTOIRE_DES_COMMANDES.put("feed-back", new FeedBack());
         REPERTOIRE_DES_COMMANDES.put("feedback", new FeedBack());
-        REPERTOIRE_DES_COMMANDES.put("setPseudo", new SetPseudoNomGuilde());
-        REPERTOIRE_DES_COMMANDES.put("setPseudoNomGuilde", new SetPseudoNomGuilde());
-        REPERTOIRE_DES_COMMANDES.put("setPseudoIDGuilde", new SetPseudoIDGuilde());
-        REPERTOIRE_DES_COMMANDES.put("setPseudoIDChannel", new SetPseudoIDGuilde());
-        REPERTOIRE_DES_COMMANDES.put("setPseudoNomChannel", new SetPseudoNomChannel());
+        REPERTOIRE_DES_COMMANDES.put("setpseudo", new SetPseudoNomGuilde());
+        REPERTOIRE_DES_COMMANDES.put("setpseudonomguilde", new SetPseudoNomGuilde());
+        REPERTOIRE_DES_COMMANDES.put("setpseudoidguilde", new SetPseudoIDGuilde());
+        REPERTOIRE_DES_COMMANDES.put("setpseudoidchannel", new SetPseudoIDChannel());
+        REPERTOIRE_DES_COMMANDES.put("setpseudonomchannel", new SetPseudoNomChannel());
     }
 
     /**
