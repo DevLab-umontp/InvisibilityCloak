b78c46254d14b3d8fe474ab7286bfe9d4d642756
MathieuSoysal <Mathieu.soysal@outlook.fr>
2021-03-08 21:36:53 +0100

    🚑 Correction problème de connexion BD

--- src/main/java/fr/umontpellier/iut/bd/BaseDonnee.java
+++ src/main/java/fr/umontpellier/iut/bd/BaseDonnee.java
@@ -9,8 +9,6 @@ import java.util.Properties;
 import java.util.Scanner;
 import java.util.TimeZone;
 
-import com.mysql.cj.jdbc.Driver;
-
 import fr.umontpellier.iut.GestionnairePseudonyme;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
@@ -21,7 +19,8 @@ public class BaseDonnee {
         throw new IllegalStateException("Utility class");
     }
 
-    private static Connection connection;
+    private static Properties connectionProps;
+    private static String url;
 
     /**
      * @param connection the connection to set
@@ -45,18 +44,14 @@ public class BaseDonnee {
 
     public static void setConnection(String userName, String password, String dbms, String serverName,
             String portNumber, String nomBD) {
-        Properties connectionProps = new Properties();
+        connectionProps = new Properties();
         connectionProps.put("user", userName);
         connectionProps.put("password", password);
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = String.format(
+            url = String.format(
                     "jdbc:%s://%s:%s/%s?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=%s",
                     dbms, serverName, portNumber, nomBD, TimeZone.getDefault().getID());
-            connection = DriverManager.getConnection(url, connectionProps);
-        } catch (SQLException e) {
-            System.out.println("La connexion à la Base de Donnée n'a pas pu être effectué !");
-            e.printStackTrace();
         } catch (ClassNotFoundException e) {
             System.out.println("Le fichier .jar n'as pas pus être trouvé");
             e.printStackTrace();
@@ -64,7 +59,8 @@ public class BaseDonnee {
     }
 
     public static void initBaseDonneePseudos() {
-        try (Statement stmt = connection.createStatement()) {
+        try (Connection connection = DriverManager.getConnection(url, connectionProps);
+                Statement stmt = connection.createStatement()) {
             stmt.execute(RequetesSQL.DROP_TABLE_PSEUDOS);
             stmt.execute(RequetesSQL.CREATE_TABLE_PSEUDOS);
             stmt.execute(RequetesSQL.INIT_TABLE_PSEUDOS);
@@ -73,7 +69,7 @@ public class BaseDonnee {
         }
     }
 
-    public static String getPseudoSinonAttribuePseudoEtAvertieUser(TextChannel channel, User user) {
+    public static String getPseudoSinonAttribuerUnPseudoEtAvertirUser(TextChannel channel, User user) {
         final int AUCUN_PSEUDO_TROUVE = -1;
         long idUser = user.getIdLong();
         long idGuild = channel.getGuild().getIdLong();
@@ -87,7 +83,8 @@ public class BaseDonnee {
     }
 
     public static int getPseudoIndex(long idUser, long idGuild) {
-        try (Statement stmt = connection.createStatement()) {
+        try (Connection connection = DriverManager.getConnection(url, connectionProps);
+                Statement stmt = connection.createStatement()) {
             ResultSet rs = stmt.executeQuery(RequetesSQL.selectIndexPseudo(idUser, idGuild));
             if (rs.next())
                 return rs.getInt("indexPseudo");
@@ -99,7 +96,8 @@ public class BaseDonnee {
 
     public static int setPseudo(long idUser, long idGuild) {
         int indexPseudo = -1;
-        try (Statement stmt = connection.createStatement()) {
+        try (Connection connection = DriverManager.getConnection(url, connectionProps);
+                Statement stmt = connection.createStatement()) {
             ResultSet rs = stmt.executeQuery(RequetesSQL.recuperePseudoNonUtilise(idGuild));
             if (rs.next()) {
                 indexPseudo = rs.getInt(1);
@@ -117,7 +115,8 @@ public class BaseDonnee {
     }
 
     public static void deleteAffectationPseudo(long idUser, long idGuild) {
-        try (Statement stmt = connection.createStatement()) {
+        try (Connection connection = DriverManager.getConnection(url, connectionProps);
+                Statement stmt = connection.createStatement()) {
             stmt.execute(RequetesSQL.deleteAffection(idUser, idGuild));
         } catch (SQLException e) {
             e.printStackTrace();
